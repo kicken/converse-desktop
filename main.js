654c9ff69e8f47e4213af88dfe59906dedd58cac
Keith Maika <keithm@aoeex.com>
2025-09-11 00:35:34 -0400

    Add a context menu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, ipcMain, shell, dialog } = require('electron')
+const {app, BrowserWindow, ipcMain, shell, dialog} = require('electron')
 const path = require('path');
+const contextMenu = require('electron-context-menu').default;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -16,12 +17,15 @@ const themeService = require(__dirname + '/modules/theme-service')
 const isMac = process.platform === 'darwin'
 const isWin = process.platform === 'win32'
 
-function initApp () {
+function initApp() {
     if (!app.requestSingleInstanceLock()) {
         app.quit();
     }
 
     try {
+        contextMenu({
+            showSearchWithGoogle: false
+        });
         createWindow()
         // Set Windows platform notifications
         if (isWin) {
@@ -34,9 +38,9 @@ function initApp () {
     }
 }
 
-function createWindow () {
-    function getSavedWindowBounds () {
-        const winBounds = settingsService.get('winBounds', { width: 800, height: 600 });
+function createWindow() {
+    function getSavedWindowBounds() {
+        const winBounds = settingsService.get('winBounds', {width: 800, height: 600});
         winBounds.width = Math.max(winBounds.width, 200);
         winBounds.height = Math.max(winBounds.height, 200);
         return winBounds;
@@ -90,7 +94,7 @@ function createWindow () {
     // Handle shutdown event on Mac with minimizeOnClose
     // to prevent shutdown interrupt
     if (isMac) {
-        const { powerMonitor } = require('electron')
+        const {powerMonitor} = require('electron')
         powerMonitor.on('shutdown', () => {
             app.isQuitting = true
             app.quit()
@@ -116,7 +120,7 @@ function createWindow () {
         shell.openExternal(details.url).catch((reason) => {
             console.log(reason);
         });
-        return { action: 'deny' };
+        return {action: 'deny'};
     })
 
     settingsService.webContents = mainWindow.webContents;
